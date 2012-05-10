205030e61da05711edb4632f54486f1638f4f56c
alepaez <alepaezseq@gmail.com>
2012-05-10 15:21:26 -0300

    moved Rule to rule.rb

--- lib/rate_limiting.rb
+++ lib/rate_limiting.rb
@@ -1,53 +1,7 @@
 require "json"
+require "rule"
+
 class RateLimiting
-
-  class Rule
-    def initialize(options)
-      default_options = {
-        :match => /.*/,
-        :metric => :rph,
-        :type => :frequency,
-        :limit => 100
-      }
-      @options = default_options.merge(options)
-      
-    end
-
-    def match
-      @options[:match].class == String ? Regexp.new(@options[:match] + "$") : @options[:match]
-    end
-
-    def limit
-      (@options[:type] == :frequency ? 1 : @options[:limit])
-    end
-
-    def get_expiration
-      (Time.now + ( @options[:type] == :frequency ? get_frequency : get_fixed )).strftime("%d%m%y%H%M%S")
-    end
-
-    def get_frequency
-      case @options[:metric]
-      when :rpd
-        return (86400/@options[:limit] == 0 ? 1 : 86400/@options[:limit])
-      when :rph
-        return (3600/@options[:limit] == 0 ? 1 : 3600/@options[:limit])
-      when :rpm
-        return (60/@options[:limit] == 0 ? 1 : 60/@options[:limit])
-      end
-    end
-
-    def get_fixed
-      case @options[:metric]
-      when :rpd
-        return 86400
-      when :rph
-        return 3600
-      when :rpm
-        return 60
-      end
-    end
-  end
-
 
   def initialize(app, &block)
     @app = app
