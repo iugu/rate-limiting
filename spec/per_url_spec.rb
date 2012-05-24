ebc3228d84d1f7f5647ef8fbc07cf9deaca4e348
alepaez <alepaezseq@gmail.com>
2012-05-24 09:26:49 -0300

    per_url rule option

---
+++ spec/per_url_spec.rb
@@ -0,0 +1,34 @@
+require "spec_helper"
+
+describe "per_url rule" do
+  include Rack::Test::Methods
+
+  context "true" do
+
+    it 'should not allow equal urls' do
+      get '/per_url/url1', {}, {'HTTP_ACCEPT' => "text/html"}
+      get '/per_url/url1', {}, {'HTTP_ACCEPT' => "text/html"}
+      last_response.body.should show_not_allowed_response
+    end
+    
+    it 'should allow different urls' do
+      get '/per_url/url1', {}, {'HTTP_ACCEPT' => "text/html"}
+      get '/per_url/url2', {}, {'HTTP_ACCEPT' => "text/html"}
+      last_response.body.should show_allowed_response
+    end
+
+  end
+
+  context "false" do
+
+    it 'should not allow different urls' do
+      get '/per_match/url1', {}, {'HTTP_ACCEPT' => "text/html"}
+      get '/per_match/url2', {}, {'HTTP_ACCEPT' => "text/html"}
+      last_response.body.should show_not_allowed_response
+    end
+  
+  end
+
+  
+
+end
